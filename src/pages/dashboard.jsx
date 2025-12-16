a4ee48a3dede8ae9ea8b93d5a3a062634e9ffb10
sandrinaalfira <sandrinaalfira@gmail.com>
2025-12-16 17:26:20 +0700

    State in Component with Hooks

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,19 +1,40 @@
-import React from 'react'
-import MainLayout from '../components/Layouts/MainLayout'
+import React from "react";
+import MainLayout from "../components/Layouts/MainLayout";
+import CardBalance from "../components/Fragments/CardBalance";
+import CardGoal from "../components/Fragments/CardGoal";
+import CardUpcomingBill from "../components/Fragments/CardUpcomingBill";
+import CardRecentTransaction from "../components/Fragments/CardRecentTransaction";
+import CardStatistic from "../components/Fragments/CardStatistic";
+import CardExpenseBreakdown from "../components/Fragments/CardExpenseBreakdown";
+import { transactions, bills, expensesBreakdowns } from "./data";
 
-function dashboard() {
+function Dashboard() {
+  console.log(transactions);
+
   return (
-    <>
     <MainLayout>
-
-      <div>dashboard</div>
-
+      <div className="grid sm:grid-cols-12 gap-6">
+        <div className="sm:col-span-4">
+          <CardBalance />
+        </div>
+        <div className="sm:col-span-4">
+          <CardGoal />
+        </div>
+        <div className="sm:col-span-4">
+          <CardUpcomingBill data={bills} />
+        </div>
+        <div className="sm:col-span-4 sm:row-span-2">
+          <CardRecentTransaction data={transactions} />
+        </div>
+        <div className="sm:col-span-8">
+          <CardStatistic />
+        </div>
+        <div className="sm:col-span-8">
+          <CardExpenseBreakdown data={expensesBreakdowns} />
+        </div>
+      </div>
     </MainLayout>
-
-    </>
-
-  )
-
+  );
 }
 
-export default dashboard;
+export default Dashboard;
